ac39cca5c1e87fd84e042dc4b099873eba2eca24
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-13 15:28:23 -0500

    remove test

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,3 @@ class Author < ActiveRecord::Base
   has_many :books
   has_many :genres, through: :books
 end
-
-
-# This is text
-# THIS IS MORE STUFF
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,3 @@ class User < ActiveRecord::Base
   has_many :book_users
   has_many :books, through: :book_users
 end
-
-BLobbity Blahahahaha
