a82a190a571aeafb98c104c28329861eda766152
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-14 15:55:22 -0500

    avoid crash on no author

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,7 +4,11 @@ require 'pry'
 
 def get_book_data_from_title(title, author)
   #make the web request
+  if(author)
   response_string = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=intitle:#{title}+inauthor:#{author}")
+else
+  response_string = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=intitle:#{title}")
+end
   response_hash = JSON.parse(response_string)
   data = response_hash["items"][0]["volumeInfo"] # get description
   title= data["title"]
