4c9a9d3a1c4e0103eadabe0afe998a06a0a4e06a
github username <github email address>
2018-11-14 10:51:44 -0500

    Changes to API Communicator

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,20 +2,11 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_book_data_from_title(title)
+def get_book_data_from_title(title, author)
   #make the web request
-  # response_string = RestClient.get('http://www.swapi.co/api/people/')
-  # response_hash = JSON.parse(response_string)
+  response_string = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=intitle:#{title}+inauthor:#{author}")
+  response_hash = JSON.parse(response_string)
 
-  # binding.pry
+  desc = response_hash["items"][0]["volumeInfo"]
 
 end
-
-# def print_movies(films_hash)
-#   # some iteration magic and puts out the movies in a nice list
-# end
-
-# def show_character_movies(character)
-#   films_array = get_character_movies_from_api(character)
-#   print_movies(films_array)
-# end
